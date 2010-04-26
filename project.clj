005e4c565498b63233b927e7e801e63732f69812
Norbert Schneider <Norbert-Schneider@gmx.de>
2010-04-26 21:33:19 +0200

    Removed unused dependeny.

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "Little translator which uses Googles Translation API."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]                 
-                 [swank-clojure "1.1.0"]
-                 [clojure-http-client "1.0.0-SNAPSHOT"]])
+                 [swank-clojure "1.1.0"]])
